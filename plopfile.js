07b4713993fbb5d6f17f5056ae1613f1433293cd
tongzhirong <tongzhirong@bizvane.cn>
2021-04-09 12:04:17 +0800

    利用Plop生成模板代码

---
+++ plopfile.js
@@ -0,0 +1,40 @@
+const moment = require('moment')
+module.exports = function(plop) {
+    plop.setGenerator('create', {
+        description: '创建一个新的md文件',
+        prompts: [
+            {
+                type: 'input',
+                name: 'title',
+                message: '请输入标题',
+                default: '标题',
+            },
+            {
+                type: 'input',
+                name: 'date',
+                message: '日期',
+                default: moment().format('YYYY-MM-DD'),
+            },
+            {
+                type: 'list',
+                name: 'tag',
+                message: '请选择标签',
+                choices: ['CSS', 'Javascript', 'Vue', 'Webpack', '生活'],
+            },
+        ],
+        actions: (data) => {
+            const date = '{{date}}'
+            const actions = [
+                {
+                    type: 'add',
+                    path: `docs/${date}.md`,
+                    templateFile: 'plop-templates/view/index.hbs',
+                    data: {
+                        name: date,
+                    },
+                },
+            ]
+            return actions
+        },
+    })
+}
